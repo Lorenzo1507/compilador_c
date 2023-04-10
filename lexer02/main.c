54e845e2a030674ee673bc00929746438c655642
Lorenzo1507 <61765932+Lorenzo1507@users.noreply.github.com>
2023-04-10 10:46:09 -0300

    erros corrigidos/código funcional

--- lexer02/main.c
+++ lexer02/main.c
@@ -4,110 +4,129 @@
 
 /*enum é uma definição de dados que o usuário faz. Ele pode ser usado para para
 transformar em tipo certos inteiros, strings e chars.
-Por exemplo: tenho "abc", logo através do enum eu considero ele como tipo "minhasLestras"*/
-enum TokenType {
-    IDENTIFICADOR,
-    MAIS,
-    MENOS,
-    MULTIPLIC,
-    POTENCIA,
-    DIVISAO   
+Por exemplo: tenho "abc", logo através do enum eu considero ele como tipo
+"minhasLestras"*/
+enum TokenType { 
+  INVALIDO, 
+  MAIS, 
+  MENOS, 
+  MULTIPLIC, 
+  POTENCIA, 
+  DIVISAO 
 };
 
 static const char *TokenTypeStrings[] = {
-    "IDENTIFICADOR",
-    "MAIS",
-    "MENOS",
-    "MULTIPLIC",
-    "POTENCIA",
-    "DIVISAO"   
+  "IDENTIFICADOR", 
+  "MAIS", 
+  "MENOS", 
+  "MULTIPLIC", 
+  "POTENCIA", 
+  "DIVISAO"
 };
 
+int main(int argc, char **argv) {
+  // mais espaço só por garantia
+  char *token = (char *)malloc(3 * sizeof(char));
 
-int main(int argc, char *argv[]) {
-    char *text;
-    int text_length;
-    
-    //mais espaço só por garantia 
-    char *token = malloc(3*sizeof(char));
+  enum TokenType type;
 
-    enum TokenType type;
+  // Read in the code from a file
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    return 1;
+  }
 
-    // Read in the code from a file
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return 1;
+  FILE *fp = fopen(argv[1], "rb");
+
+  if (fp == NULL) {
+    fprintf(stderr, "Unable to open file '%s'\n", argv[1]);
+    return 1;
+  }
+
+  int text_length;
+
+  fseek(fp, 0L, SEEK_END);
+  text_length = ftell(fp);
+  fseek(fp, 0L, SEEK_SET);
+
+  char *text = (char *)calloc(text_length, sizeof(char));
+  fread(text, sizeof(char), text_length, fp);
+  fclose(fp);
+
+  text[text_length] = '\0';
+
+  int cont = 0;
+  // Interpret the code
+  for (int i = 0; i < text_length; i++) {
+    // printf("%c\n", text[i]);
+    switch (text[i]) {
+    case '+':
+      type = MAIS;
+      token[0] = text[i];
+      //printf("Token: [\"%s\", Tipo: %s]\n", token, TokenTypeStrings[type]);
+      break;
+
+    case '-':
+      type = MENOS;
+      token[cont] = text[i];
+      //printf("Token: [\"%s\", Tipo: %s]\n", token, TokenTypeStrings[type]);
+      break;
+
+    case '*':
+      if (text[i + 1] != '\0' && text[i + 1] == '*')
+      {
+        type = POTENCIA;
+        token[cont] = text[i];
+        
+        i++;
+        cont++;
+        
+        token[cont] = text[i];
+      } 
+      else 
+      {
+        type = MULTIPLIC;
+        token[cont] = text[i];
+      }
+
+      //printf("Token: [\"%s\", Tipo: %s]\n", token, TokenTypeStrings[type]);
+      break;
+
+    case '/':
+      type = DIVISAO;
+      token[cont] = text[i];
+      //printf("Token: [\"%s\", Tipo: %s]\n", token, TokenTypeStrings[type]);
+      break;
+
+    /*case ' ':
+      i++;
+      break;
+
+    case '\n':
+      i++;
+      break;
+
+    case '\t':
+      i++;
+      break;*/
+
+    default:
+      type = INVALIDO;
+      token[cont] = text[i];
+      //printf("Token: [\"%s\", Tipo: %s]\n", token, TokenTypeStrings[type]);
+      break;
     }
+    printf("Token: [\"%s\", Tipo: %s]\n", token, TokenTypeStrings[type]);
+    // para limpar os conteúdos da string
+    memset(token,0,strlen(token));
+    cont = 0;
+  }
 
-    FILE *fp = fopen(argv[1], "r");
-    if (fp == NULL) {
-        fprintf(stderr, "Unable to open file '%s'\n", argv[1]);
-        return 1;
-    }
+  free(text);
+  free(token);
 
-    fseek(fp, 0, SEEK_END);
-    text_length = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
+  //"make" para dar build no código -> e depois "./main lexer.txt" para rodar o
+  //código
 
-    text = malloc(text_length + 1);
-    fread(text, 1, text_length, fp);
-    fclose(fp);
-
-    text[text_length] = '\0';
-
-    
-
-    // Interpret the code
-    for (int i = 0; i < text_length; i++) 
-    {
-        switch (text[i]) {
-            case '+':
-                type = MAIS;
-                token = text[i];
-                printf("Token: [\"%s\", Tipo: %s]\n", token, TokenTypeStrings[type]);
-                break;
-
-            case '-':
-                type = MENOS;
-                token = text[i];
-                printf("Token: [\"%s\", Tipo: %s]\n", token, TokenTypeStrings[type]);
-                break;
-
-            case '*':
-                if(text[i+1] != '\0' && text[i+1] == '*')
-                {
-                    type = POTENCIA;
-                    token = text[i];
-                    token += text[i+1];
-                }
-                else
-                {
-                    type = MULTIPLIC;
-                    token = text[i];
-                }
-                
-                printf("Token: [\"%s\", Tipo: %s]\n", token, TokenTypeStrings[type]);              
-                break;
-
-            case '/':
-                type = DIVISAO;
-                token = text[i];
-                printf("Token: [\"%s\", Tipo: %s]\n", token, TokenTypeStrings[type]);
-                break;
-
-            default:
-                type = IDENTIFICADOR;
-                token = text[i];
-                printf("Token: [\"%s\", Tipo: %s]\n", token, TokenTypeStrings[type]);
-                break;           
-        }
-
-        //para limpar os conteúdos da string
-        memset(token,0,strlen(token));
-    }
-
-    free(text);
-    free(token);
-
-    return 0;
+  return 0;
 }
